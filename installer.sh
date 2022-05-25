3739728d3277e4ca836faaa2562ffdaac1e4b99d
Creative-Difficulty <80907917+Creative-Difficulty@users.noreply.github.com>
2022-05-25 18:38:15 +0200

    clear workspace

--- installer.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-curl https://github.com/creative-difficulty/eucoapiclient/releases/latest &&
-unzip EucoAPIClient.zip &&
-cd EucoAPIClient &&
-npm i &&
-echo "Done installing EucoAPIClient!"
