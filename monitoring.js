84aae3a5111e63c19653bd20235ed737e36cfc3f
Creative-Difficulty <80907917+Creative-Difficulty@users.noreply.github.com>
2022-06-29 19:35:17 +0200

    refactor code

--- monitoring.js
+++ monitoring.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
+
+import { exit } from "process";
+import fetch from "node-fetch"
+import {fetchEucoAPI} from "./js/functions.js";
 //TODO: migrate to react
 import fs from "fs"
-import fetch from "node-fetch"
-import path from "path";
 import logger from "node-color-log"
-import { exit } from "process";
+import path from "path";
 import { zip } from "zip-a-folder";
-import {fetchEucoAPI} from "./js/functions.js";
 const __dirname = path.resolve()
 
 logger.setLevel("success")
-// if(isPi()) {
-//     console.log("EucoAPIClient is running on a Raspberry Pi !\n Initializing Pi-only features")
-// } else {
-//     console.log("EucoAPIClient is running on a desktop computer")
-// }
 
 process.argv.shift();
 process.argv.shift();
@@ -114,67 +110,6 @@ async function fetchJSON() {
     }
 
     console.log(recievedJSON)
-    
-    if(LoggedNum > 0) {
-        if(recievedJSON.s === true) {
-            var jsonTime = recievedJSON.t
-            var jsonCpuUsage = recievedJSON.cu
-            var jsonPerformanceMS = recievedJSON.pt
-        } else {
-            return;
-        }
-        
-        
-        var jsonRAM = recievedJSON["RAM"]
-        var jsonUsedRAM = jsonRAM["usedMemMb"]
-        var jsonFreeRAM = jsonRAM["freeMemMb"]
-        
-        
-        var jsonStorage = recievedJSON["storage_info"]
-        var jsonUsedStorage = jsonStorage["usedGb"]
-        var jsonFreeStorage = jsonStorage["freeGb"]
-        
-        
-        
-        parseableData = "time=" + jsonTime + "," + "cpu_usage=" + jsonCpuUsage + "," + "used_ram=" + jsonUsedRAM + "," + "free_ram=" + jsonFreeRAM + "," + "free_storageGB=" + jsonFreeStorage + "," + "used_storageGB=" + jsonUsedStorage + "\n"
-
-    } else if(LoggedNum === 0) {
-        var jsonTime = recievedJSON["time"]
-        var jsonCpuUsage = recievedJSON["cpu_usage"]
-        
-        var jsonRAM = recievedJSON["RAM"]
-        var jsonTotalRAM = jsonRAM["totalMemMb"]
-        var jsonUsedRAM = jsonRAM["usedMemMb"]
-        var jsonFreeRAM = jsonRAM["freeMemMb"]
-        
-        var jsonCPUinfo = recievedJSON["cpu_type"]
-        var jsonCPUCores = jsonCPUinfo[1]
-        var jsonCPUmodel = jsonCPUCores["model"]
-        var jsonCPUSpeed = jsonCPUCores["speed"]
-        
-        var jsonStorage = recievedJSON["storage_info"]
-        var jsonStorageCapacity = jsonStorage["totalGb"]
-        var jsonUsedStorage = jsonStorage["usedGb"]
-        var jsonFreeStorage = jsonStorage["freeGb"]
-        
-        var jsonOSinfo = recievedJSON["os_version"]
-        var jsonOSname = jsonOSinfo["codename"]
-        var jsonOSVersion = jsonOSinfo["release"]
-        var jsonOSarchitecture = jsonOSinfo["arch"]
-        var jsonOSlocalIP = jsonOSinfo["hostname"]
-        var jsonOStextEncoding = jsonOSinfo["codepage"]
-        var jsonOSserialnumber = jsonOSinfo["serial"]
-        var jsonOSuefiboolean = jsonOSinfo["uefi"]
-
-        if(jsonOSserialnumber === null) {
-            jsonOSserialnumber = "not available"
-        }
-        
-        parseableData = "time=" + jsonTime + "," + "cpu_model=" + jsonCPUmodel + "," + "cpu_speed=" + jsonCPUSpeed + "," + "cpu_usage=" + jsonCpuUsage + "," + "total_ram=" + jsonTotalRAM + "," + "used_ram=" + jsonUsedRAM + "," + "free_ram=" + jsonFreeRAM + "," + "total_storageGB=" + jsonStorageCapacity + "," + "free_storageGB=" + jsonFreeStorage + "," + "used_storageGB=" + jsonUsedStorage + "," + "os_name=" + jsonOSname + "," + "os_version=" + jsonOSVersion + "," + "os_architecture=" + jsonOSarchitecture + "," + "local_ip=" + jsonOSlocalIP + "," + "pc_serial_number=" + jsonOSserialnumber + "," + "uefi_is_enabled=" + jsonOSuefiboolean + "," + "os_text_encoding=" + jsonOStextEncoding + "\n"
-        
-        
-    }
-    LoggedNum++;
     
     
     if(parseableData !== null || parseableData !== undefined) {
